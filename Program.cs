25912f0576e91d76bd50fc327ea1bdca52fa9e52
Acrobaticbag3 <kevintom214@gmail.com>
2022-12-08 12:32:12 +0100

    Loop error

--- Program.cs
+++ Program.cs
@@ -4,7 +4,19 @@ using System.Collections.Generic;
 namespace Filhantering {
     internal class progarm {
         public static void Main(string[] args) {
-            
+            string[] lines = File.ReadAllLines(path: "./talfil.txt");
+            string[] intager;
+            int newNum = 0;
+
+            foreach (string line in lines) {
+                intager = lines;
+                int[] ints = Array.ConvertAll(array: lines, converter: s => int.Parse(s: s));
+
+                foreach (int num in ints) {
+                    newNum += ints[num];
+                    Console.WriteLine(value: newNum);
+                }
+            }
         }
     }
 }
